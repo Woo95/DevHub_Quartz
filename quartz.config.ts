3a616f6b82337b74d080eaa9c20327df81ab0470
Woo95 <chaewan2002@gmail.com>
2025-06-01 06:26:54 +0900

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "https://woo95.github.io/Devhub/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
